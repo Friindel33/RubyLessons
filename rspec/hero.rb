f3a4f83c701ef33c2fd21a24f27d4cb9e0fef0d9
Juri F <friindel@mail.ru>
2020-06-13 13:25:41 +0100

    added Hero entity

--- rspec/hero.rb
+++ rspec/hero.rb
@@ -1,2 +1,13 @@
 class Hero
+  def initialize(name, health=100)
+    @name = name.capitalize
+    @health = health
+  end
+
+  def name
+    @name
+  end
 end
+
+hero = Hero.new 'ironman'
+puts hero.name
