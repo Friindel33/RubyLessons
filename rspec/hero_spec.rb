9fa9ced3c6833ecae6bb517251ed18bf2f7b0b13
Juri F <friindel@mail.ru>
2020-06-15 11:41:54 +0100

    tests has been refactored

--- rspec/hero_spec.rb
+++ rspec/hero_spec.rb
@@ -1,24 +1,24 @@
 require './hero'
 
 describe Hero do
-  it "has a capitalized name" do
-    hero = Hero.new 'ironman'
 
-    expect(hero.name).to eq 'Ironman' #hero.name == "Ironman"
+  before do
+    @hero = Hero.new 'ironman'
+  end
+
+  it "has a capitalized name" do
+    expect(@hero.name).to eq 'Ironman' #hero.name == "Ironman"
   end
 
   it "can power up" do
-    hero = Hero.new 'ironman'
-    expect(hero.power_up).to eq 110
+    expect(@hero.power_up).to eq 110
   end
 
   it "can power down" do
-    hero = Hero.new 'thor'
-    expect(hero.power_down).to eq 90
+    expect(@hero.power_down).to eq 90
   end
 
   it "displays full hero info" do
-    hero = Hero.new 'bobby'
-    expect(hero.hero_info).to eq 'Bobby has 100% of health'
+    expect(@hero.hero_info).to eq 'Ironman has 100% of health'
   end
 end
