60e19d7f03e6e9afbd522ce96a43d6db5913f9d7
Juri F <friindel@mail.ru>
2020-06-15 11:23:07 +0100

    2 extra tests has been added

--- rspec/hero_spec.rb
+++ rspec/hero_spec.rb
@@ -11,4 +11,14 @@ describe Hero do
     hero = Hero.new 'ironman'
     expect(hero.power_up).to eq 110
   end
+
+  it "can power down" do
+    hero = Hero.new 'thor'
+    expect(hero.power_down).to eq 90
+  end
+
+  it "displays full hero info" do
+    hero = Hero.new 'bobby'
+    expect(hero.hero_info).to eq 'Bobby has 100% of health'
+  end
 end
